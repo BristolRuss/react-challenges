e18a830571624b5667d58691be0d8afb69edd6fe
BristolRuss <BristolRuss@gmail.com>
2020-03-02 12:59:27 +0000

    adding updated App.js file and stuff component - challenge 05

--- james-app/src/App.js
+++ james-app/src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Paragraph from './components/Paragraph';
-import Square from './components/Square';
-import People from './components/People';
+import Stuff from './components/Stuff';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Header/> 
         <img src={logo} className="App-logo" alt="logo" />
-        <Paragraph/>
-        <Square/>
-        <People/>
+        <Stuff/>
       </header>
     </div>
   );
