60387280316bde139423cd85b30a4f60b45c9521
BristolRuss <BristolRuss@gmail.com>
2020-03-02 16:26:35 +0000

    updated prop name for last challenge

--- james-app/src/components/People.js
+++ james-app/src/components/People.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const People = ({arrayOfNames}) => {
-    return !arrayOfNames ? <p>Nothing to see here</p> : (
+const People = ({names}) => {
+    return !names ? <p>Nothing to see here</p> : (
     <ul>
-    {arrayOfNames.map((value, index) => (
+    {names.map((value, index) => (
         <li key={index}>{value}</li>
     ))}
     </ul>
--- james-app/src/components/Stuff.js
+++ james-app/src/components/Stuff.js
@@ -9,7 +9,7 @@ const Stuff = () => (
     <Header text="Things with cats" />
     <Paragraph message="Cats are wonderful"/>
     <Square colour="purple" height="300px" width= "400px" />
-    <People arrayOfNames = {["James", "Rebecca", "Amanda", "John", "Brenda", "Tony", "Sue"]}/>
+    <People names = {["James", "Rebecca", "Amanda", "John", "Brenda", "Tony", "Sue"]}/>
     
     
 </>
