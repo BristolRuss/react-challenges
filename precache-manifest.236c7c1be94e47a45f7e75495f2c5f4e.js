4cac68f78d4449c420968e5f3eab8e5d8ff3bcc0
BristolRuss <BristolRuss@gmail.com>
2020-03-06 12:13:30 +0000

    Updates

---
+++ precache-manifest.236c7c1be94e47a45f7e75495f2c5f4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43bb6937479e62e60024b23bbb4cce5",
+    "url": "/%3Crepo%3E/index.html"
+  },
+  {
+    "revision": "03c3710ba97f40c66eb2",
+    "url": "/%3Crepo%3E/static/css/main.0c0cdd20.chunk.css"
+  },
+  {
+    "revision": "e01d42faccf846edb480",
+    "url": "/%3Crepo%3E/static/js/2.7a7695bb.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/%3Crepo%3E/static/js/2.7a7695bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c3710ba97f40c66eb2",
+    "url": "/%3Crepo%3E/static/js/main.d86b268a.chunk.js"
+  },
+  {
+    "revision": "01ccb81ea4ecc7df1eae",
+    "url": "/%3Crepo%3E/static/js/runtime-main.ecbf9668.js"
+  }
+]);
